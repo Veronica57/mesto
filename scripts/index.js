fbadee343d7bc5fa0ba69139a977f54af69e9fe6
Veronika Pinchuk <tveronichka@gmail.com>
2023-02-26 23:51:15 +0300

    JS fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,21 @@
 const openEditPopupButton = document.querySelector(".profile__edit-button");
-
-if (!openEditPopupButton) {
-    throw new Error("No openEditPopupButton!");
-}
-
 const closePopupButton = document.querySelector(".popup__exit");
-if (!closePopupButton) {
-    throw new Error("No closePopupButton!");
-}
-
 const editPopup = document.querySelector(".popup");
-if (!editPopup) {
-    throw new Error("No editPopup!");
-}
-
+const userName = document.querySelector(".profile__name");
+const userDescription = document.querySelector(".profile__description");
+const userNameInput = document.querySelector(".popup__form-input_user_name");
+const userDescriptionInput = document.querySelector(
+    ".popup__form-input_user_description"
+);
+const formElement = document.querySelector(".popup__form");
 // Open popup
 function openPopup(popup) {
     popup.classList.add("popup_active");
 }
-
 // Close popup
 function closePopup(popup) {
     popup.classList.remove("popup_active");
 }
-
-openEditPopupButton.addEventListener("click", function () {
-    openPopup(editPopup);
-});
-
-closePopupButton.addEventListener("click", function () {
-    closePopup(editPopup);
-});
-
-const user = {
-    name: "Жак-Ив Кусто",
-    description: "Исследователь океана",
-};
-
-const userName = document.querySelector(".profile__name");
-
-const userDescription = document.querySelector(".profile__description");
-
-const userNameInput = document.querySelector(".popup__form-input_name");
-userNameInput.value = user.name;
-
-const userDescriptionInput = document.querySelector(
-    ".popup__form-input_description"
-);
-userDescriptionInput.value = user.description;
-
-const formElement = document.querySelector(".popup__form");
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -59,3 +25,13 @@ function handleFormSubmit(evt) {
 }
 
 formElement.addEventListener("submit", handleFormSubmit);
+
+openEditPopupButton.addEventListener("click", function () {
+    userNameInput.value = userName.textContent;
+    userDescriptionInput.value = userDescription.textContent;
+    openPopup(editPopup);
+});
+
+closePopupButton.addEventListener("click", function () {
+    closePopup(editPopup);
+});
