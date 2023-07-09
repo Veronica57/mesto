ec45ca06811e9c8f27386348ab1f82480eb2a5d6
Veronika Pinchuk <tveronichka@gmail.com>
2023-07-10 01:57:56 +0300

    webpack & babel configiration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { query } = require("express");
 module.exports = {
     entry: { main: "./src/pages/index.js" },
     output: {
         path: path.resolve("./dist"),
-        filename: "main.js",
+        filename: "scripts/[name].js",
         publicPath: "",
     },
     mode: "development",
@@ -14,4 +17,40 @@ module.exports = {
         port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
         open: true, // сайт будет открываться сам при запуске npm run dev
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: { importLoaders: 1 },
+                    },
+                    "postcss-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[hash][ext][query]",
+                },
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[hash][ext][query]",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "styles/[name].css" }),
+    ],
 };
